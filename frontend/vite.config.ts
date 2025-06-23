c56b4bb67dc2994b60ede21322b543982014a32a
GuruF <guruprasath@aidaptive.com>
2025-06-23 21:54:49 +0530

    Fix postMessage

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,17 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(),
-  {
-    name: 'add-cors',
-    configureServer(server) {
-      server.middlewares.use((_req, res, next) => {
-        res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-        res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-        res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
-        next();
-      });
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
     },
   },
-  ],
 })
